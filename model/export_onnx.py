272bd1e4badededde3db4e9dc5a16a475cab301a
yashi0209 <rajayushi0209@gmail.com>
2025-04-21 21:14:11 -0700

    exported pytorch weight to onnx weights

--- model/export_onnx.py
+++ model/export_onnx.py
@@ -1,17 +1,22 @@
 import torch
 from model import CustomCNN
-from conf import pytorch_model_state_file_name, onnx_model_state_file_name
 
-# Instantiate and prepare the model
+# filenames
+pytorch_model_state_file_name = "E:\Files\ASU\sem_4\disml_final_project\spark-udf-cnn-inference\model\id_classification_cnn_model.pth"
+onnx_model_state_file_name = "id_classification_cnn_model.onnx"
+
+# 1. Instantiate your model (matches how you trained it)
 model = CustomCNN(num_classes=3)
-model.initialize_fc()  # Initialize FC layer
-model.load_state_dict(torch.load(pytorch_model_state_file_name))
+
+# 2. Load the weights (use map_location if needed)
+state = torch.load(pytorch_model_state_file_name, map_location="cpu")
+model.load_state_dict(state)
 model.eval()
 
-# Create dummy input for ONNX tracing
+# 3. Create a dummy input with the same shape you trained on
 dummy_input = torch.randn(1, 3, 128, 128)
 
-# Export to ONNX
+# 4. Export to ONNX
 torch.onnx.export(
     model,
     dummy_input,
@@ -22,4 +27,4 @@ torch.onnx.export(
     opset_version=11,
 )
 
-print("✅ Exported model to model.onnx")
+print(f"✅ Exported model to {onnx_model_state_file_name}")
